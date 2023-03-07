7c2be22c6deb1a693faa1fd2661ca680f22b62ba
alejandro <soto.millan.ceni@gmail.com>
2023-03-06 23:39:08 -0500

    update 06-03 2

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -1,9 +1,5 @@
 const URL_API = process.env.REACT_APP_URL_API;
 
-
 export const endpoints = {
-
-    nations: `${URL_API}nations`,
-    flights: `${URL_API}flights`
-
+    nations: `${URL_API}nations`
 }
